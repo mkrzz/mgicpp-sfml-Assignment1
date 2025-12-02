a15864cc335b0df38b05a604d5ad4302425cbedd
Matt <Matthew2.Krzywon@live.uwe.ac.uk>
2025-12-02 15:03:24 +0000

    Changed frame size to 1920 x 1080

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main()
             << std::endl;
 
   // create window and set up
-  sf::RenderWindow window(sf::VideoMode(1080, 720), "Game");
+  sf::RenderWindow window(sf::VideoMode(1920, 1080), "Game");
   window.setFramerateLimit(60);
 
   //initialise an instance of the game class
